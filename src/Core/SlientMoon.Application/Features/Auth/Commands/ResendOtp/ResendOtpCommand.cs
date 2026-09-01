f75ed0c8ba338d38b46f50d5dff614c9c6fd60a4
aytan08 <aytanzd@code.edu.az>
2026-09-01 19:06:30 +0400

    minor changes

--- src/Core/SlientMoon.Application/Features/Auth/Commands/ResendOtp/ResendOtpCommand.cs
+++ src/Core/SlientMoon.Application/Features/Auth/Commands/ResendOtp/ResendOtpCommand.cs
@@ -1,7 +1,8 @@
 ﻿using Application.Abstractions.Messaging;
+using SlientMoon.Application.DTOs.Auth;
 
 namespace SlientMoon.Application.Features.Auth.Commands.ResendOtp;
 
 public sealed record ResendOtpCommand(
     string Email
-) : ICommand;
+) : IBaseCommand;
